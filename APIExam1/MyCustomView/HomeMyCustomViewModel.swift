00a6298ee36476228b948b93991795883ed64293
AA <>
2023-12-18 20:49:03 -0600

    MVVM HomeMyCustom

---
+++ APIExam1/MyCustomView/HomeMyCustomViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewModel.swift
+//  APIExam1
+//
+//  Created by Angel Tekiani Duarte Estrada on 18/12/23.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+   var coordinator : HomeViewCoordinator!
+    
+}
+
